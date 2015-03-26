e84be0362fb11c982844da3b64dfd4051c666659
Eduardo Shanahan <contact@eduardoshanahan.com>
2015-03-26 14:54:06 +0000

    Removed resources

--- README.md
+++ README.md
@@ -9,15 +9,28 @@ Vagrant
 Git
 
 To be able to run the example the first step after getting the source code is to get the Fabric scripts resources from Github:
-
 ```
 fab git_clone
 ```
 
 Then you can go to code and get the vagrant machine that will be our build server
-
 ```
 cd code
 fab prepare_machine
 
+```
+
+Now you need the source code in the new vagrant instance:
+```
+fab at_vagrant get_source_code
+```
+
+The build itself will be:
+```
+fab at_vagrant build
+```
+
+All in one line:
+```
+fab at_vagrant get_source_code build
 ```
--- code/fabfile.py
+++ code/fabfile.py
@@ -3,13 +3,11 @@ from fabric.api import env
 from fabric.api import run
 from fabric.api import sudo
 from fabric.api import task
-
+import datetime
 
 env.application_name = 'spikes_build'
 env.deployment_directory = '/opt'
-env.build_directory = 'build'
-# env.source_code_directory = 'code_from_git'
-
+env.application_code_directory = 'code'
 
 @task
 def at_vagrant():
@@ -49,105 +47,17 @@ def get_source_code():
     """
     Call git and get the source code
     """
-    # run('mkdir -p {0}'.format(env.source_code_directory))
     run('git clone https://github.com/eduardoshanahan/spikes_build')
+
 
 @task
 def build():
     """
     Get the code from VCS, get it ready to work and push a tag back to VCS
     """
-    with cd(env.source_code_directory):
-        sudo('mkdir -p {0}'.format(env.build_directory))
-
-
-# @task
-# def install_kafka():
-#     """
-#     Get the kafka server ready
-#     """
-#     import sys
-#     sys.path.insert(0, '../../../resources/')
-#     from fabric_scripts.scripts import ubuntu, kafka
-#     ubuntu.packages.update()
-#     kafka.full(configuration='../configuration/kafka')
-#     create_kafka_topics()
-#     ubuntu.packages.cleanup()
-#
-#
-# @task
-# def install_servers(hosts_ip='127.0.0.1', hosts_name='kafka'):
-#     """
-#     Get the tools for working in the source code
-#     """
-#     import sys
-#     sys.path.insert(0, '../../../resources/')
-#     from fabric_scripts.scripts import ubuntu, nginx, nodejs
-#     ubuntu.packages.update()
-#     ubuntu.hosts.add(hosts_ip, hosts_name)
-#     ubuntu.hosts.add('127.0.0.1', 'Logstash') # At the moment keep Logstash on the same machine
-#     nginx.full(configuration='../configuration/nginx')
-#     nodejs.install()
-#     ubuntu.packages.cleanup()
-#
-#
-# @task
-# def install_load():
-#     """
-#     Get ready to load test the servers
-#     """
-#     import sys
-#     sys.path.insert(0, '../../../resources/')
-#     from fabric_scripts.scripts import ubuntu
-#     ubuntu.packages.update()
-#     python.locust.full()
-#     ubuntu.packages.cleanup()
-#
-#
-# @task
-# def install_log():
-#     """
-#     Log management
-#     """
-#     import sys
-#     sys.path.insert(0, '../../../resources/')
-#     from fabric_scripts.scripts import ubuntu
-#     ubuntu.packages.update()
-#     logstash.full(configuration='../configuration/logstash')
-#     elasticsearch.full(configuration='../configuration/elasticsearch')
-#     kibana.full(configuration='../configuration/kibana')
-#     ubuntu.packages.cleanup()
-#
-#
-# @task
-# def install_single_machine():
-#     """
-#     All the services in a single machine
-#     """
-#     import sys
-#     sys.path.insert(0, '../../../resources/')
-#     from fabric_scripts.scripts import ubuntu, nginx, kafka, nodejs
-#     ubuntu.packages.update()
-#     ubuntu.hosts.add('127.0.0.1', 'Kafka')
-#     ubuntu.hosts.add('127.0.0.1', 'Logstash')
-#     nginx.full(configuration='../configuration/nginx')
-#     kafka.full(configuration='../configuration/kafka')
-#     # logstash.full(configuration='../configuration/logstash')
-#     # elasticsearch.full(configuration='../configuration/elasticsearch')
-#     # kibana.full(configuration='../configuration/kibana')
-#     create_kafka_topics()
-#     nodejs.full()
-#     nodejs.tools.dtrace.install()
-#     ubuntu.packages.cleanup()
-#
-#
-# @task
-# def create_kafka_topics():
-#     """
-#     Create all the topics the platform needs in Kafka
-#     """
-#     topics = ['company', 'contract', 'supplier', 'user']
-#     with cd('/usr/lib/kafka'):
-#         for topic in topics:
-#             run('bin/kafka-topics.sh --create --zookeeper Kafka:2181 --replication-factor 1 --partitions 1 --topic {0}'.format(topic))
-#         run(' bin/kafka-topics.sh --list --zookeeper Kafka:2181')
+    with cd(env.application_name), cd (env.application_code_directory):
+        run('npm install')
+        version = 'build_{0}'.format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M'))
+        # print('version is {0}'.format(version))
+        run('git tag {0}'.format(version))
+        run('git push origin {0}'.format(version))
